a38916c641a952e7bb28e35be07f626380b64fea
miyaguni-rikuto <120348141+miyaguni-rikuto@users.noreply.github.com>
2024-08-19 10:24:07 +0900

    ステージ背景の土台作成

---
+++ OC/StageBack.cpp
@@ -0,0 +1,22 @@
+#include "StageBack.h"
+#include "DxLib.h"
+
+StageBack::StageBack()
+{
+	//AsaImg = LoadGraph();
+}
+
+StageBack::~StageBack()
+{
+
+}
+
+void StageBack::Update()
+{
+
+}
+
+void StageBack::Draw()
+{
+
+}
---
+++ OC/StageBack.h
@@ -0,0 +1,19 @@
+#pragma once
+class StageBack
+{
+
+	int AsaImg;
+	int HiruImg;
+	int YoruImg;
+	
+	
+public:
+	StageBack();
+
+	~StageBack();
+
+	void Update();
+
+	void Draw();
+};
+
